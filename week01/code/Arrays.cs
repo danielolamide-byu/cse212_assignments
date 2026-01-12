859517ce4bbdcf01464cda6238a35d46ccd2d6d0
danielolamide-byu <olamidedaniel874@gmail.com>
2026-01-12 23:21:45 +0100

    Dynamic Arrays.

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //  ------------IMPLEMENTATION---------- -
 
-        return []; // replace this return statement with your own
+        //  To solve this problem I initialized an array that holds doubles. Then a variable 'i' in a 'for' loop that runs while the 
+        //  variable 'i' is less than the product of number of multiples(length) we want to get
+        //   and the start number(number) and then increment the variable 'i'  by the start number
+        //   and then return the array.
+
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = (i + 1) * number;
+        }
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -23,11 +34,19 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+        //  ------------IMPLEMENTATION---------- -
+    ///  
+    /// // I created a variable and used the 'Skip' function on the list and passed the number of items in the list
+    // (data.Count) minus the amount we want to rotate the list by as the parameter.
+    // Then I created another variable and used the 'Take' function on the list and passed in the same parameter.
+    // Then call the 'Clear' fuction on the list.
+    // Then claaed the 'AddRange' function on the list and passed the two variables created earlier as parameter.
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        var rotatedPart = data.Skip(data.Count - amount).ToList();
+        var remainingPart = data.Take(data.Count - amount).ToList();
+        data.Clear();
+        data.AddRange(rotatedPart);
+        data.AddRange(remainingPart);
     }
 }
